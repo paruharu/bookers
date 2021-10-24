0a0b1a8958dde1e6e278f77e9de687d18e857a64
paruharu <oha.ru.sunny0861@gmail.com>
2021-10-24 02:34:24 +0000

    1024

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,11 @@
 class BooksController < ApplicationController
+
   def index
+    @books = Book.all
   end
 
   def show
-   @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def new
@@ -11,15 +13,36 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully updated."
     # showに戻る
-    redirect_to book_path
+     redirect_to book_path(@book.id)
+    else
+     render :index
+    end
+
   end
 
   def edit
     @book = Book.find(params[:id])
   end
+
+　def destroy
+　  book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+　end
+
+　def update
+　  @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book)
+    else
+      render :edit
+　  end
+　end
 
   private
   def book_params
